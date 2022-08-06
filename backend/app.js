ace78233edf88d6ec068da786609f5aac2064530
Maayan Journo <maayanjourno@gmail.com>
2022-08-06 18:14:38 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const auth = require('./middleware/auth');
 const { ErrorHandler, customErrorHandler } = require('./errors/error');
 const { requestLogger, errorLogger } = require('./middleware/logger');
 const { errors } = require('celebrate');
+const cors = require("cors");
 
 // NODE_ENV=production
 // JWT_SECRET=eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b
@@ -36,6 +37,13 @@ app.use(requestLogger);
 
 //   next();
 // });
+const allowedOrigins = [
+  "https://majo.students.nomoreparties.sbs",
+  "https://www.majo.students.nomoreparties.sbs",
+  "https://api.majo.students.nomoreparties.sbs",
+];
+app.use(cors());
+app.options(allowedOrigins, cors());
 
 
 app.get('/', (req, res) => {
